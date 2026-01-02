25bc4801a383e34b2058cf6219dedcb146790f2e
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2026-01-02 08:14:29 +0700

    + simple cors policy and client ajax to load foods

--- backend/src/Program.cs
+++ backend/src/Program.cs
@@ -5,11 +5,12 @@ builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<FoodDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IFoodRepository, FoodRepository>();
-
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 var app = builder.Build();
 app.UseExceptionHandler("/exception");
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors();
     //app.UseDeveloperExceptionPage();
     app.MapOpenApi();
 }
